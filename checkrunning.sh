ffc65163e06cca9e73d0010f22604f2a543a7e97
Jorge Scarenzi <138737038+jorgescarenzi@users.noreply.github.com>
2023-10-23 01:20:19 -0400

    Update checkrunning.sh

--- checkrunning.sh
+++ checkrunning.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-for id in $(docker ps -q)
+for id in $(docker ps -q -a)
 do
     if [[ $(docker name "${id}") == *"${1}"* ]]; then
         echo "stopping container ${id}"
         docker stop "${id}"
+        docker rm "${id}"
     fi
 done
