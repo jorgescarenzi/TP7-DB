1e50bc7910b008413b7d6f7c4e67d641163e4494
Jorge Scarenzi <138737038+jorgescarenzi@users.noreply.github.com>
2023-10-22 23:14:02 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if ls -l /var/lib/mysql/ecomdb/products.frm 2> /dev/null > /dev/null
+if ls -l var/lib/mysql/ecomdb/products.ibd 2> /dev/null > /dev/null
 then
   echo  "TEST PASSED ---------------------"
 
